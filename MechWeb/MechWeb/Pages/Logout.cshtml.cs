a845a24c41d6cfa2baf9d4452f4c3d0e2dcb2223
sandyordanov <a.yordanov@student.fontys.nl>
2023-05-26 01:16:03 +0200

    Logout functionality working

--- MechWeb/MechWeb/Pages/Logout.cshtml.cs
+++ MechWeb/MechWeb/Pages/Logout.cshtml.cs
@@ -1,6 +1,7 @@
 using DataLibrary;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
 
 namespace MechWeb.Pages.Shared
 {
@@ -9,7 +10,7 @@ namespace MechWeb.Pages.Shared
         public readonly UserDbController _dbController = new UserDbController();
         public void OnGet()
         {
-            
+            Response.Cookies.Delete("userId");
             Response.Redirect("/Index");
         }
     }
